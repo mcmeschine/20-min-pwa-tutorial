9299b80f70d046159cacd74d7c633bf5f6600a72
Marie Claire <Marie Claire@DESKTOP-EBJ9VAP.fra.glb.tru.com>
2020-01-24 11:29:27 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/20-min-pwa-tutorial`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -18,17 +19,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/20-min-pwa-tutorial`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
